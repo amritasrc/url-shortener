2ca55369ed991a5d9dfa2cdf5c4dc23c3efe2718
amritasrc <amritakumari13022006@gmail.com>
2026-08-30 18:06:14 +0530

    feat: track URL visits and add analytics endpoint

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 const express = require('express');
-const {connectToMongoDB} = require('./connect')
+const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url');
-
+const URL = require('./models/url')
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
-.then(()=>console.log("MongoDB connected!"))
-.catch(()=>console.error("An error occured."))
+    .then(() => console.log("MongoDB connected!"))
+    .catch(() => console.error("An error occured."))
+
+app.use(express.json());
 
 app.use("/url", urlRoute);
 
-app.listen(PORT, ()=>{
+app.get('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate({
+        shortId
+    },
+        {
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            },
+        }
+    );
+    res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => {
     console.log(`Server started at PORT: http://localhost:${PORT} `);
-    
-})
+
+});
