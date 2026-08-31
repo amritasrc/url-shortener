dc535c2c93b9f7912347b632f73eea5ec5f0e178
amritasrc <amritakumari13022006@gmail.com>
2026-08-31 21:47:10 +0530

    feat: server side rendering with ejs

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const path = require('path');
 const { connectToMongoDB } = require('./connect')
 const urlRoute = require('./routes/url');
+const staticRouter = require('./routes/staticRouter')
+
 const URL = require('./models/url')
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
     .then(() => console.log("MongoDB connected!"))
-    .catch(() => console.error("An error occured."))
+    .catch(() => console.error("An error occured."));
+
+app.set('view engine', "ejs");
+app.set('views', path.resolve('./views'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+app.use("/", staticRouter);
 
-app.get('/:shortId', async (req, res) => {
+app.get('/url/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
         shortId
@@ -26,6 +34,11 @@ app.get('/:shortId', async (req, res) => {
             },
         }
     );
+
+    if (!entry) {
+        return res.status(404).send('Short URL not found');
+    }
+
     res.redirect(entry.redirectURL);
 });
 
