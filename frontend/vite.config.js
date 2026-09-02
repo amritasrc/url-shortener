80b557ade743201c873bebadd4abdb98686ae138
amritasrc <amritakumari13022006@gmail.com>
2026-09-02 19:40:09 +0530

    feat: conncet vite to backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,4 +12,14 @@ export default defineConfig({
       }
     },
   },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
