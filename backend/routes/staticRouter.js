24ba5e1c8cd47c57312ae97088d519fa981b68fc
amritasrc <amritakumari13022006@gmail.com>
2026-09-07 09:57:15 +0530

    chore: removed ejs

--- backend/routes/staticRouter.js
+++ backend/routes/staticRouter.js
@@ -3,10 +3,10 @@ const URL = require('../models/url')
 
 const router = express.Router();
 
-router.get('/', async (req, res)=> {
+router.get('/', async (req, res) => {
     const allurls = await URL.find({});
-    return res.render('home', {
-        urls: allurls
+    router.get('/', (req, res) => {
+        res.send('URL Shortener API is running');
     });
 });
 
